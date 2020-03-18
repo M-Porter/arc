ac781580904b60e3ff1fe8c7ce6f7da80fbe0bf5
Matt Porter <mporter@squareup.com>
2020-03-18 15:27:16 -0700

    Added git stuff

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/m-porter/arc
 go 1.14
 
 require (
+	github.com/go-git/go-git/v5 v5.0.0
 	github.com/spf13/cobra v0.0.6
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.2.4
 )
--- lib/sync/sync.go
+++ lib/sync/sync.go
@@ -2,8 +2,11 @@ package sync
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/m-porter/arc/lib/config"
 	"github.com/m-porter/arc/lib/util"
+	"strings"
 )
 
 func ProjectByName(name string, force bool) error {
@@ -38,6 +41,49 @@ func Project(project *config.Project, force bool) error {
 }
 
 func Service(service *config.Service, force bool) error {
+	repo, err := git.PlainOpen(service.Path)
+	if err != nil {
+		return fmt.Errorf("git error: %v", err)
+	}
+
+	currentBranch, err := currentBranchForService(repo)
+	if err != nil {
+		return fmt.Errorf("git error: %v", err)
+	}
+
+	if currentBranch != service.Branch {
+		return fmt.Errorf("service branch %s not equal to current checked out branch %s... skipping", service.Branch, currentBranch)
+	}
+
 	util.Printlnf("syncing service %s", service.Name)
+
 	return nil
+}
+
+func currentBranchForService(repo *git.Repository) (string, error) {
+	branchRefs, err := repo.Branches()
+	if err != nil {
+		return "", err
+	}
+
+	headRef, err := repo.Head()
+	if err != nil {
+		return "", err
+	}
+
+	var currentBranchName string
+	err = branchRefs.ForEach(func(branchRef *plumbing.Reference) error {
+		if branchRef.Hash() == headRef.Hash() {
+			currentBranchName = branchRef.Name().String()
+			return nil
+		}
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	currentBranchName = strings.Replace(currentBranchName, "refs/heads/", "", 1)
+
+	return currentBranchName, nil
 }
